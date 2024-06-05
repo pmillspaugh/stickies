efdd9fada9ca1d3b3ebed25cbbd0f1bcb8c763a9
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2024-06-05 15:02:08 -0400

    Init stickies app from eframe template

--- src/app.rs
+++ src/app.rs
@@ -67,7 +67,7 @@ impl eframe::App for TemplateApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+            ui.heading("stickies");
 
             ui.horizontal(|ui| {
                 ui.label("Write something: ");
